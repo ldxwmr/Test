a8bb63877310423563b12cf1e450deec2fb2648f
798299596@qq.com <798200506@qq.com>
2021-11-25 14:46:20 +0800

    algorithm pritice1

---
+++ algorithm/src/main/java/org/example/两数相加/ListNode.java
@@ -0,0 +1,31 @@
+package org.example.两数相加;
+
+/**
+ * @Author ldxwmr
+ * @Date 2021/11/25 9:27
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
