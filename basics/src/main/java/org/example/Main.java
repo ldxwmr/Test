9055cf76c83fba1ce1dab59ebdbd1410d438de31
v-xuzhe@frasersproperty.com <v-xuzhe@frasersproperty.com>
2023-03-22 14:16:34 +0800

    map深拷贝、浅拷贝练习

--- basics/src/main/java/org/example/Main.java
+++ basics/src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "C (31 Jan 2023)";
-        String c = s.replaceAll("C", "");
-
-
         System.out.println("Hello world!");
     }
 }
---
+++ basics/src/main/java/org/example/MapTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapTest {
+    public static void main(String[] args) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        map.put("a", list);
+        List<String> list1 = new ArrayList<>();
+        list1.add("2");
+        Map<String, List<String>> map1 = new HashMap<>();
+        map1.put("b",list1);
+        map1.putAll(map);
+        Map<String, List<String>> map2 = map;
+        map.clear();
+        map = null;
+        System.out.println(map);
+
+    }
+}
