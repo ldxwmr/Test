cb7e225c14df1e8ff2d6b3eec6845a47eeed16f4
798200506@qq.com <798200506@qq.com>
2024-10-20 19:52:17 +0800

    多个远程仓库练习

--- io/src/main/java/org/example/file/CreateFile.java
+++ io/src/main/java/org/example/file/CreateFile.java
@@ -7,6 +7,7 @@ public class CreateFile {
         System.out.println("test");
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
     }
 
 }
